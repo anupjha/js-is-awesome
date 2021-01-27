08952711d45ae4b0a12cd4eacc8925998d6c65ed
Anup Jha <anupkrjha@gmail.com>
2021-01-27 12:36:34 +0530

    Update object.create.js

--- js-snippet/object.create.js
+++ js-snippet/object.create.js
@@ -1,19 +1,3 @@
-if (typeof Object.create !== "function") {
-  Object.create = function (o, props) {
-    function F() {}
-    F.prototype = o;
-
-    if (typeof props === "object") {
-      for (prop in props) {
-        if (props.hasOwnProperty(prop)) {
-          F[prop] = props[prop];
-        }
-      }
-    }
-    return new F();
-  };
-}
-
 // Object.create Partial Polyfill
 // Support for second parameter is non-standard
 if (typeof Object.create !== "function") {
@@ -36,23 +20,4 @@ if (typeof Object.create !== "function") {
     // Return new object
     return result;
   };
-}
-
-// From MDN
-if (typeof Object.create != "function") {
-  Object.create = (function () {
-    var Temp = function () {};
-    return function (prototype) {
-      if (arguments.length > 1) {
-        throw Error("Second argument not supported");
-      }
-      if (typeof prototype != "object") {
-        throw TypeError("Argument must be an object");
-      }
-      Temp.prototype = prototype;
-      var result = new Temp();
-      Temp.prototype = null;
-      return result;
-    };
-  })();
 }
